12dd86ac13004b88e022e34476835224eeb3b8da
최준영 <chad1994@skuniv.ac.kr>
2018-12-04 16:20:00 +0900

    create project

---
+++ app/src/main/java/kr/ac/skuniv/choejun_yeong/boostcamp3/MainActivity.java
@@ -0,0 +1,13 @@
+package kr.ac.skuniv.choejun_yeong.boostcamp3;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
